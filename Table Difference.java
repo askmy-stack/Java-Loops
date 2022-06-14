9a5c6645fc0d7182cb498af5820edb42895d0db4
Abhinaysai Kamineni <66816045+askmy-stack@users.noreply.github.com>
2022-06-14 16:50:59 +0530

    Create Table Difference.java

---
+++ Table Difference.java
@@ -0,0 +1,28 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Practice {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the First Number:");
+            int a = sc.nextInt();
+        System.out.print("Enter the Second Number:");
+            int b = sc.nextInt();
+            TableDifference(a,b);
+    }
+    public static void TableDifference(int a, int b){
+//        for (int i = 1; i <=10 ; i++) {
+//            System.out.println(((a-b)*i)+ " ");
+//        }
+
+
+//        int i = 1;
+//        while(i<=10){
+//            System.out.println(((a-b)*i)+ " ");
+//            i++;
+//        }
+    }
+}
+
+//Can be implemented using for and while loops.
